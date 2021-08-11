1ea9262301f963e0d4afebda76f6702cbde75402
zhangs41 <zhangs41@qq.com>
2021-08-11 23:09:05 +0800

    first commit as a security project

---
+++ src/main/java/com/example/security/repo/RoleRepo.java
@@ -0,0 +1,8 @@
+package com.example.security.repo;
+
+import com.example.security.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
